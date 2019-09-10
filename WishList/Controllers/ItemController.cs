07fe2904140b0d59dc7dc0cfdd3ad3a6add8e7b0
Jean-Francois Landry <jflandry@prevost-architectural.com>
2019-09-10 15:59:25 -0400

    Update ItemController.cs

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -37,7 +37,7 @@ namespace WishList.Controllers
 
         public IActionResult Delete(int id)
         {
-            var = _context.Items.FirstOrDefault(e => e.Id == id);
+            var item = _context.Items.FirstOrDefault(e => e.Id == id);
             _context.Items.Remove(item);
             _context.SaveChanges();
             return RedirectToAction("Index");
